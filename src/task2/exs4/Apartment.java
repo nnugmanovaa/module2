d433b7363de4d5bb4dc91f3d47934420bd7d55c6
Madelkhanova <Madelxanova@mail.ru>
2024-01-09 23:13:12 +0600

    Home Work 2

---
+++ src/task2/exs4/Apartment.java
@@ -0,0 +1,8 @@
+package task2.exs4;
+
+public class Apartment {
+    private String title;
+    private int smet;
+    private String type;
+
+}
---
+++ src/task2/exs4/Colony.java
@@ -0,0 +1,9 @@
+package task2.exs4;
+
+public class Colony {
+    private String name;
+    private int smetPrice;
+    private int apCount;
+    Apartment[] aparts;
+
+}
